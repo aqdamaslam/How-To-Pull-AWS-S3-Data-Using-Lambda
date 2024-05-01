7938b1b61c35a2d5486db18f5bfd8d11e22bd4d4
Aqdam Aslam <56361347+aqdamaslam@users.noreply.github.com>
2024-05-01 13:39:13 +0530

    Update LambdaCode.py

--- LambdaCode.py
+++ LambdaCode.py
@@ -1,17 +1,17 @@
 import boto3
 import pandas as pd
 
-s3_client = boto3.client('s3')
+s3_file = boto3.client('s3')
 
 def lambda_handler(event, context):
     # TODO implement
     print(event)
     try:
-        bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-        s3_file_key = event["Records"][0]["s3"]["object"]["key"]
-        print(bucket_name)
-        print(s3_file_key)
-        resp = s3_client.get_object(Bucket=bucket_name, Key=s3_file_key)
+        bucketName = event["Records"][0]["s3"]["bucket"]["name"]
+        s3Key = event["Records"][0]["s3"]["object"]["key"]
+        print(bucketName)
+        print(s3Key)
+        resp = s3_file.get_object(Bucket=bucketName, Key=s3Key)
         print(resp['Body'])
         df_s3_data = pd.read_csv(resp['Body'], sep=",")
         print(df_s3_data.head())
